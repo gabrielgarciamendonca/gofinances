54c61bb7088190db670627f31a2185e932d154d5
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-11-07 21:28:27 -0300

    feat: First commit preparing project

---
+++ src/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+interface DashboardProps {}
+
+export function Dashboard({}: DashboardProps) {
+  return (
+    <Text>Esta no Dashboard</Text>
+  )
+}
